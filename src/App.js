481e228cfeff90b25d91339587cdd2c77b619852
Dr. Paul Kenneth Shreeman <pkshreeman@gmail.com>
2017-06-19 13:20:04 -0700

    Slowly progressing.
    Improved overall function layout, and logic process.  Still haven’t
    completed that part yet.

--- src/App.js
+++ src/App.js
@@ -95,13 +95,9 @@ function createRandomSelection (current){
     //console.log(randomcolor);
     //this.setState({matchMe:[...this.state.matchMe, randomcolor]});
     let selector = ['green',"blue",'purple',"pink"];
-
     selected.push(selector[Math.floor(Math.random()*selector.length)]);
-    console.log("selected")
-  //  return selected;
-
   }
-  current.setState({matchMe: selected});
+  current.setState({matchMe: selected}, function(){demoMatch(current.state)});
 }
 
 
@@ -112,32 +108,36 @@ function drawCirclesOn(current) {
   drawcircle("purple", current.purple);
 }
 //Start of logic for Simon
-function theLogic(current){
-  console.log("theLogic function is triggered.")
+function theLogic(current) {
+  //console.log("theLogic function is triggered.")
   // 20 times the game is over //
-  if (current.userInput.length == 20){
+  if (current.userInput.length == 20) {
     alert("Congratulations! You have won!");
     document.getElementById('switch').click();
-  }
-  else{
-    console.log("This is current userInput: " + current.userInput);
-    console.log("This is matchMe array: " + current.matchMe);
-    console.log("The Length of userInput is "+ current.userInput.length)
-
-    for(let i = 0; i < current.userInput.length; i++){
-      if (current.userInput[i] != current.matchMe[i]){
-      console.log("the loop is at " + i);
-      alert("You idiot!");
-      if(current.strict == 'STRICT'){
-      document.getElementById('reset').click()}
+  } else {
+    // Checking if the input matches...
+    for (let i = 0; i < current.userInput.length; i++) {
+      if (current.userInput[i] != current.matchMe[i]) {
+        console.log("the loop is at " + i);
+        alert("You idiot!");
+        if (current.strict == 'STRICT') {
+          document.getElementById('reset').click()
+        }
       }
     }
   }
-  // If strict, reset
-
-  //can you return a 'setState'?
 }
 //End of Logic for Simon
+//demo function
+function demoMatch(current){ //enter this.state as current
+  let cycle = current.userInput.length;
+  for( let i = 0 ; i < cycle + 1 ; i++){
+    setTimeout(() => drawcircle(current.matchMe[i],current.matchMe[i]), 500);
+    setTimeout(() => drawcircle(current.matchMe[i], current[current.matchMe[i]]), 800);
+  }
+}
+
+// Start of React Component
 class CreateCanvas extends React.Component {
   constructor(props) {
     super(props);
@@ -164,48 +164,52 @@ class CreateCanvas extends React.Component {
 
   //Captures all the clicking activities within the React Component
   handleClick(event) {
-    let clickedId = event.target.id;
+      let clickedId = event.target.id;
 
-    //console.log("The clicked element's id is " + clickedId);
+      if (clickedId == 'switch') {
+        if (this.state.switch === "OFF") {
+          this.setState({
+            "switch": 'ON',
+            'purple': '#d5aee2',
+            'green': '#60ff70',
+            'blue': '#6dd0ff',
+            'pink': '#ffeaf6',
+          })
+          createRandomSelection(this);
+
+        } else {
+          this.setState({
+            "switch": 'OFF',
+            "green": 'green',
+            "blue": 'blue',
+            "purple": 'purple',
+            "pink": "pink",
+          })
+          https: //stackoverflow.com/questions/6367339/trigger-a-button-click-from-a-non-button-element
+            document.getElementById('reset').click();
+        };
+      }
+
+      //console.log("The clicked element's id is " + clickedId);
 
 
-    if (clickedId === 'green' || clickedId === 'blue' || clickedId === 'pink' || clickedId === 'purple') {
-      //console.log("Only green, blue, pink, purple should trigger this");
-      if(this.state.switch == 'ON'){
-      setTimeout(() => drawcircle(clickedId, clickedId), 50);
-      setTimeout(() => drawcircle(clickedId, this.state[clickedId]), 300);
-      //https://stackoverflow.com/questions/26253351/correct-modification-of-state-arrays-in-reactjs
-      this.setState({userInput:[...this.state.userInput, clickedId]});
-      //console.log(this.state.userInput)
-    }
-  }
+      if (clickedId === 'green' || clickedId === 'blue' || clickedId === 'pink' || clickedId === 'purple') {
+        //console.log("Only green, blue, pink, purple should trigger this");
+        if (this.state.switch == 'ON') {
+          setTimeout(() => drawcircle(clickedId, clickedId), 50);
+          setTimeout(() => drawcircle(clickedId, this.state[clickedId]), 300);
+          //https://stackoverflow.com/questions/26253351/correct-modification-of-state-arrays-in-reactjs
+          this.setState({
+            userInput: [...this.state.userInput, clickedId]
+          });
+          //console.log(this.state.userInput)
+          theLogic(this.state);
+        }
+      }
 
 
-    else if (clickedId == 'switch') {
-    if (this.state.switch === "OFF") {
-      this.setState({
-        "switch": 'ON',
-        'purple': '#d5aee2',
-        'green': '#60ff70',
-        'blue': '#6dd0ff',
-        'pink': '#ffeaf6',
-      })
-      createRandomSelection(this);
-
-    } else {
-      this.setState({
-        "switch": 'OFF',
-        "green" : 'green',
-        "blue" : 'blue',
-        "purple": 'purple',
-        "pink": "pink",
-      })
-      https://stackoverflow.com/questions/6367339/trigger-a-button-click-from-a-non-button-element
-      document.getElementById('reset').click();
-      };
-  }
-
-  else if (clickedId == 'strict') {
+if (clickedId == 'strict') {
+  if (this.state.switch == 'ON') {
   if (this.state.strict == "STRICT") {
     this.setState({
       "strict": 'NotStrict',
@@ -215,14 +219,17 @@ class CreateCanvas extends React.Component {
       "strict": 'STRICT'
     })
   };
-}
+}}
 
 else if (clickedId =='reset'){
+  if (this.state.switch == 'ON') {
   this.setState({
     "strict": 'NotStrict',
     "matchMe": [],
     "userInput": []
   })
+  createRandomSelection(this)
+}
 }
   }
 
@@ -243,7 +250,7 @@ else if (clickedId =='reset'){
 
 componentDidUpdate(){
   drawCirclesOn(this.state);
-  theLogic(this.state);
+
 }
 }
 
